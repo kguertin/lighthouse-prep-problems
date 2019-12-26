d91ae2866680fdbfb7f0d186784fe689ff5b5357
Kevin Guertin <kevin.m.guertin@gmail.com>
2019-12-26 22:40:57 +0000

    adding solution that uses for loop rather than array sorting

--- sum-largest-numbers.js
+++ sum-largest-numbers.js
@@ -1,3 +1,5 @@
+//Using array sorting
+/*
 let sumLargestNumbers = function(data) {
   let num = data.sort(function(a,b){
     return b - a;
@@ -15,7 +17,35 @@ let sumLargestNumbers = function(data) {
     
     }
   }
+  */
+
+//Using for loop
+let sumLargestNumbers = function(data){
+  let largestNum = data[0],
+      secondLargestNum = data[1];
+
+  if(data.length < 2){
+    return 'Error'
+  }
+
+  if(secondLargestNum > largestNum){
+    largestNum = data[1];
+    secondLargestNum = data[0];
+  }
+
+  for(let i = 0 ; i< data.length; i++){
+    if(data[i] > largestNum){
+      secondLargestNum = largestNum;
+      largestNum = data[i];
+    } else if (data[i] > secondLargestNum){
+      secondLargestNum = data[i];
+    }
+  }
+
+  return largestNum + secondLargestNum;
+}
+
 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); 
