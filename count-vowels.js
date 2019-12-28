bb36dfb92933f32848b716fa8627f43af4268c33
Kevin Guertin <kevin.m.guertin@gmail.com>
2019-12-28 00:09:24 +0000

    commiting solution to find number of vowels

---
+++ count-vowels.js
@@ -0,0 +1,31 @@
+let numberOfVowels = function(data) {
+  let word = data.split('');
+  let vowelCount = 0;
+
+  for(let i  = 0; i < word.length; i++){
+    switch(word[i]){
+      case 'a':
+        vowelCount += 1;
+        break;
+      case 'e':
+        vowelCount += 1;
+        break;
+      case 'i':
+        vowelCount += 1;
+        break;
+      case 'o':
+        vowelCount += 1;
+        break;
+      case 'u':
+        vowelCount += 1;
+        break;
+    }
+  }
+
+  return vowelCount;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
+console.log(numberOfVowels("dog"));
